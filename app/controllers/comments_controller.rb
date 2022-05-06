33735b268d668ef22fa361edbb03f7f7034184a1
Piero Lescano <pierolescano@protonmail.com>
2022-05-06 10:29:20 -0500

    fix(controllers): Use instance variables to store params
    The previous syntax caused an error. See more here:
    https://github.com/piero-vic/rails-blog-app/pull/6#discussion_r866407456

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,15 +6,15 @@ class CommentsController < ApplicationController
 
   def create
     @current_user = current_user
-    user_id = params[:user_id]
-    post_id = params[:post_id]
+    @user_id = params[:user_id]
+    @post_id = params[:post_id]
 
-    comment = Comment.new(comment_params.merge(author_id: @current_user.id, post_id:))
+    comment = Comment.new(comment_params.merge(author_id: @current_user.id, post_id: @post_id))
 
     if comment.save
-      redirect_to user_post_path(user_id, post_id), notice: 'Comment published'
+      redirect_to user_post_path(@user_id, @post_id), notice: 'Comment published'
     else
-      redirect_to user_post_path(user_id, post_id), alert: 'Something went wrong'
+      redirect_to user_post_path(@user_id, @post_id), alert: 'Something went wrong'
     end
   end
 
