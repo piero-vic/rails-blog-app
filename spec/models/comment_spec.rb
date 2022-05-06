5632a18186ba6d902a7ce53fc863be82f109498b
Piero Lescano <pierolescano@protonmail.com>
2022-05-06 11:42:27 -0500

    feat(spec): Add specs for Like and Comment models

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  before do
+    User.create(id: 1, name: 'User 1')
+    Post.create(id: 1, author_id: 1, title: 'Hello 1', text: 'This is my first post')
+  end
+
+  it 'should update the post comments_counter when created' do
+    Comment.create(post_id: 1, author_id: 1, text: 'This is my first comment')
+    expect(Post.take.comments_counter).to eq 1
+  end
+end
