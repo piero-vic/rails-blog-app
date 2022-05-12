b8aecf78fb96a5475cc6d950c1be7c07d47a36ca
Piero Lescano <pierolescano@protonmail.com>
2022-05-12 16:48:37 -0500

    feat(api): Get token from API

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
 
   namespace :api, :defaults => {:format => :json} do
+    post 'login', to: 'authentication#authenticate'
     resources :users, only: [:index, :show] do
       resources :posts, only: [:index, :show] do
         resources :comments, only: [:index, :create] 
